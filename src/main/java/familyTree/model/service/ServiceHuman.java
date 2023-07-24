7684271653878930d6a9d2bf6a1d51322927d91c
AnnaVlady <7753277av@gmail.com>
2023-07-24 21:00:56 +0300

    greeting

--- src/main/java/familyTree/model/service/ServiceHuman.java
+++ src/main/java/familyTree/model/service/ServiceHuman.java
@@ -13,19 +13,24 @@ public class ServiceHuman extends MainFileHandler {
 
     FamilyTree<Human> testTree() {
         tree = new FamilyTree<>();
-        Human alex = new Human(01,"Алексей", Gender.Male, LocalDate.of(1965, 12, 4));
-        Human sveta =  new Human(02, "Светлана", Gender.Female, LocalDate.of(1967, 5, 29));
+        Human irina = new Human(01, "Ирина", Gender.Female, LocalDate.of(1944, 7, 20));
+        tree.addHuman( irina);
+        Human stepan = new Human(02,"Степан",Gender.Male,LocalDate.of(1940,1,11));
+        tree.addHuman(stepan);
+        Human alex = new Human(03,"Алексей", Gender.Male, LocalDate.of(1965, 12, 4));
+        Human sveta =  new Human(04, "Светлана", Gender.Female, LocalDate.of(1967, 5, 29),
+                stepan,irina);
         tree.addHuman(alex);
         tree.addHuman(sveta);
-        Human olga = new Human(03,"Ольга", Gender.Female, LocalDate.of(1990, 10, 25),
+//        irina.addChild(sveta);
+//        stepan.addChild(sveta);
+        Human olga = new Human(05,"Ольга", Gender.Female, LocalDate.of(1990, 10, 25),
                 alex, sveta);
-        Human ivan = new Human(04, "Иван", Gender.Female, LocalDate.of(1991, 9, 15),
+        Human ivan = new Human(06, "Иван", Gender.Female, LocalDate.of(1991, 9, 15),
                 alex, sveta);
         tree.addHuman(olga);
         tree.addHuman(ivan);
-        Human irina = new Human(05, "Ирина", Gender.Female, LocalDate.of(1944, 7, 20));
-        tree.addHuman( irina);
-        irina.addChild(sveta);
+
         return tree;
     }
     public String getHumanInfo(){
